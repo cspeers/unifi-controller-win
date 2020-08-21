0865b1b70115dfd5596b667669ad530c209274b1
unknown <Administrator@secretweapon.co>
2020-08-21 11:21:40 -0500

    update

--- build.config.ps1
+++ build.config.ps1
@@ -1,5 +1,5 @@
 $env:DOCKER_REPO = "cspeers"
 $env:DOCKER_IMAGE = "unifi"
-$env:DOCKER_APPLICATION_VERSION = "5.12.66"
+$env:DOCKER_APPLICATION_VERSION = "5.14.22"
 $env:DOCKER_IMAGE_TAG='ltsc2016-amd64'
 $env:DOCKER_BASE_IMAGE = "mcr.microsoft.com/windows/servercore:$env:DOCKER_IMAGE_TAG"
