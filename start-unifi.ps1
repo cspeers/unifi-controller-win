51894541e27d79bae68b5e3329a754f247ac06dc
Chris Speers <chris@secretweapon.co>
2020-11-11 09:36:43 -0600

    handle locks

--- start-unifi.ps1
+++ start-unifi.ps1
@@ -1,2 +1,11 @@
-Set-Location C:\Unifi\bin
-.\start.bat
+$UNIFI_PATH='C:\Unifi'
+$UNIFI_JAR=Join-Path $UNIFI_PATH '\lib\ace.jar'
+$UNIFI_DBLOCK=Join-Path $UNIFI_PATH '\data\db\WiredTiger.lock'
+$LAUNCH_ARGS=@('-Xmx1024M','-jar',$UNIFI_JAR,'start')
+if(Test-Path $UNIFI_DBLOCK){
+    Write-Host "Found Mongo LOCK. Deleting $UNIFI_DBLOCK ..."
+    Remove-Item $UNIFI_DBLOCK -Force
+}
+
+Write-Host "Starting Unifi Controller - java.exe $LAUNCH_ARGS"
+Start-Process -FilePath 'java.exe' -ArgumentList $LAUNCH_ARGS -WorkingDirectory $UNIFI_PATH -NoNewWindow -Wait
