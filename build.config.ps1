24e5410be95953ac010e43d3fcd3cb4eb0fb01b3
Chris Speers <chris@secretweapon.co>
2020-11-19 21:09:45 -0600

    version

--- build.config.ps1
+++ build.config.ps1
@@ -1,5 +1,5 @@
 $env:DOCKER_REPO = "cspeers"
 $env:DOCKER_IMAGE = "unifi"
-$env:DOCKER_APPLICATION_VERSION = "6.0.28"
+$env:DOCKER_APPLICATION_VERSION = "6.0.36"
 $env:DOCKER_IMAGE_TAG='ltsc2019-amd64'
 $env:DOCKER_BASE_IMAGE = "mcr.microsoft.com/windows/servercore:$env:DOCKER_IMAGE_TAG"
